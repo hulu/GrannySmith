152f0321a335ba7259602aeb776203bfe7af22a4
Bao Lei <bao.lei@hulu.com>
2012-06-20 16:39:23 -0700

    Corrected a typo in comment note

--- GSFancyText/src/GSFancyTextDefines.h
+++ GSFancyText/src/GSFancyTextDefines.h
@@ -16,7 +16,7 @@
 /// To check whether ARC is enabled for your project, hit cmd+1, select the project, then the target, then Build Settings, then search for Objective-C Automatic Reference Counting. If it exists and is set to Yes, ARC is enabled.
 
 
-/// Comment the following line if ARC (Automatic Reference Counting) is enabled. Uncomment it if ARC is not enabled.
+/// Uncomment the following line if ARC (Automatic Reference Counting) is enabled. Comment it if ARC is not enabled.
 // #define GS_ARC_ENABLED 1
 
 /// Comment the following line if ARC is enabled but your deployment target is iOS 4.x.
