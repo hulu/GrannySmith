72cb1d2974bdc7632d5043bac688205940c84742
Bao Lei <bao.lei@hulu.com>
2012-06-22 15:14:37 -0700

    ITWO-1629 For i2, set ARC_ENABLED and disable the warning.
    After this commit, update-index --assume-unchanged this file, so future merging with os branch will not care about this.

--- GSFancyText/src/GSConfig.h
+++ GSFancyText/src/GSConfig.h
@@ -9,13 +9,10 @@
 #ifndef GSConfig_h
 #define GSConfig_h
 
-#warning: Please set the GS_ARC_ENABLED flag according to your project setting. Then comment this line out to disable the warning.
-/// For more information about ARC, please read: http://developer.apple.com/library/mac/#releasenotes/ObjectiveC/RN-TransitioningToARC/Introduction/Introduction.html
-/// To check whether ARC is enabled for your project, hit cmd+1 in Xcode, select the project, then the target, then Build Settings, then search for Objective-C Automatic Reference Counting. If it exists and is set to Yes, ARC is enabled.
 
 
 /// Uncomment the following line if ARC (Automatic Reference Counting) is enabled. Comment it if ARC is not enabled.
-// #define GS_ARC_ENABLED 1
+ #define GS_ARC_ENABLED 1
 
 /// Comment the following line if ARC is enabled but your deployment target is iOS 4.x.
 // #define GS_ARC_WEAK_REF_ENABLED 1
